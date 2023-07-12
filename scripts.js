954fea80acca4b06f85e0014809d0c0e11b568a2
German Baena <germanb66@gmail.com>
2023-07-12 12:17:10 -0500

    Simplificando código

--- scripts.js
+++ scripts.js
@@ -1,52 +1,34 @@
+modal = document.getElementById('modal')
 
 const openModal = e => {
-
-    const templateOne = document.getElementById('template-one'),
-    templateTwo = document.getElementById('template-two'),
-    templateThree = document.getElementById('template-three')
-
-    const newTemplateOne = templateOne.content.cloneNode(true),
-    newTemplateTwo = templateTwo.content.cloneNode(true),
-    newTemplateThree = templateThree.content.cloneNode(true)
-
     e.stopPropagation()
     const id = e.target.id
 
     if (e.target.id) {
-        document.querySelector('.modal').style.opacity = 1
-        document.querySelector('.modal').style.visibility = 'visible'
+        modal.style.opacity = 1
+        modal.style.visibility = 'visible'
     }
 
     switch (id) {
         case 'button-one':
-            document.getElementById('modal').appendChild(newTemplateOne)
+            modal.appendChild(document.getElementById('template-1').content.cloneNode(true))
         break;
 
         case 'button-two':
-            document.getElementById('modal').appendChild(newTemplateTwo)
+            modal.appendChild(document.getElementById('template-2').content.cloneNode(true))
         break;
 
         case 'button-three':
-            document.getElementById('modal').appendChild(newTemplateThree)
+            modal.appendChild(document.getElementById('template-3').content.cloneNode(true))
         break;
     }
-
 }
 
 document.querySelector('.container').addEventListener('click', openModal)
 
-document.querySelector('.modal').addEventListener('click', e => {
+modal.addEventListener('click', e => {
+    modal.style.visibility = 'hidden'
     document.querySelector('.template-content').remove()
-    document.querySelector('.modal').style.visibility = 'hidden'
 })
-
-
-
-
-
-
-
-
-
 
 
